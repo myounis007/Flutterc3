324504a49f9be29b6aa2e88ccc60c141c35a3945
myounis007 <myounis4707@gmail.com>
2024-03-19 12:47:53 +0500

    ok

---
+++ map_list_practice.dart
@@ -0,0 +1,18 @@
+void main() {
+  var pracmap = [
+    {
+      1: '3r3',
+      'af': 'afffa',
+      3: [
+        {
+          'name': 'unis',
+          'adreex': 'manan',
+        },
+        2,
+        'done',
+      ]
+    },
+    2,3
+  ];
+ print(pracmap);
+}
