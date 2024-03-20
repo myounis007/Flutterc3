bc4f9bd646cd4b7a869631a33d726dd3e467c1b5
myounis007 <myounis4707@gmail.com>
2024-03-20 21:23:11 +0500

    while loop practice

--- loops.dart
+++ loops.dart
@@ -1,4 +1,20 @@
 void main() {
+  // divisor numbers
+  // int number = 344;
+  // for (var i = 1; i < number; i++) {
+  //   if (number % i == 0) {
+  //     print(i);
+  //   }
+  // }
+  // print('hello;;');
+ //adding just even number in neew list
+
+
+
+  // take two list and print numbers common between them and make sure lists are different sizes
+
+ 
+
   // for loop
   // List studentsNames = ['ali', 'ahmad', 'asad', 'atif'];
   // for (int s = 0; s < studentsNames.length; s++) {
@@ -6,30 +22,38 @@ void main() {
   // }
 
   // for in loop
-  List numbers = [
-    1,
-    23,
-    434,
-    534,
-    32,
-    2,
-    5,
-    6,
-    432,
-    2,
-    5,
-    54,
-  ];
-  List oddnumbers = [];
-  List evennumbers = [];
+  // List numbers = [
+  //   1,
+  //   23,
+  //   434,
+  //   534,
+  //   32,
+  //   2,
+  //   5,
+  //   6,
+  //   432,
+  //   2,
+  //   5,
+  //   54,
+  // ];
+  // List oddnumbers = [];
+  // List evennumbers = [];
 
-  for (int s=0;s<numbers.length;s++) {
-    if ( numbers[s]% 2 == 0) {
-      evennumbers.add(numbers[s]);
-    } else {
-      oddnumbers.add(numbers[s]);
-    }
-  }
-  print(evennumbers);
-  print(oddnumbers);
+  // for (int s=0;s<numbers.length;s++) {
+  //   if ( numbers[s]% 2 == 0) {
+  //     evennumbers.add(numbers[s]);
+  //   } else {
+  //     oddnumbers.add(numbers[s]);
+  //   }
+  // }
+  // print(evennumbers);
+  // print(oddnumbers);
+  // take a list
+  //print all the number <5 in a list
+//   List a=[1,44,232,1,2,33,4,42,2,];
+//  for (var i = 0; i <a.length; i++) {
+//   if(a[i]<5){
+// print(a[i]);
+//   }
+  //}
 }
