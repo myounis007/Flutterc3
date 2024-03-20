f51b32abed67b1a5bcfc1cc711d7b098bd7de92a
myounis007 <myounis4707@gmail.com>
2024-03-20 21:35:37 +0500

    list input praactice

--- while_dowhile_loop.dart
+++ while_dowhile_loop.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
 void main(){
- int a=0;
- while (a<10) {
-   print(a);
-   a++;
- }
+  print('please enter number or list');
+var inputnum=stdin.readLineSync();
+print(inputnum);
+
+//  int a=0;
+//  while (a<10) {
+//    print(a);
+//    a++;
+// }
 }
