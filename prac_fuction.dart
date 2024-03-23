88fbed870135d0c084586d43754cdd1b46ed2971
myounis007 <myounis4707@gmail.com>
2024-03-23 16:20:42 +0500

    ok hogiya

--- prac_fuction.dart
+++ prac_fuction.dart
@@ -1,14 +1,24 @@
-numberTable(num number){
-for (var i = 1; i <= 10; i++) {
-  print('$number  x $i = ${number*i}');
+// numberTable(num number){
+// for (var i = 1; i <= 10; i++) {
+//   print('$number  x $i = ${number*i}');
   
+// }
+// print('=======================');
+// }
+studentInfo({String? name}){
+ String? a = name;
+print(a);
 }
-print('=======================');
-}
+
 void main() {
-  List<int> number1=[12,11,2,13,3,14,4,15,6,16,7,8,9,10,];
-  for (var i in number1) {
-    numberTable(i);
-  }
+studentInfo(name: 'unis');
+
+
+
+
+  // List<int> number1=[12,11,2,13,3,14,4,15,6,16,7,8,9,10,];
+  // for (var i in number1) {
+  //   numberTable(i);
+  //}
   
 }
