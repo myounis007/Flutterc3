497c42cc48547c9e4f0f9978a16167a9a2990785
myounis007 <myounis4707@gmail.com>
2024-03-24 11:38:43 +0500

    shapes example

--- dart_project.dart
+++ dart_project.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 
-void main(){
-  for (var i = 1; i < 5; i++) {
-    for (var j= 1; j <= i; j++) {
-      stdout.write('*');
-      
+void main() {
+  var rows = 5;
+  var a = ' ';
+  for (var i = 1; i < rows; i++) {
+    stdout.write('${a * (rows - i)}');
+    for (var j = 1; j <= i; j++) {
+      stdout.write('x ');
     }
     print('');
-    
   }
 }
