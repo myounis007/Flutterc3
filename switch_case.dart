80d635f21cd60572a10f86c3f51258ae3271bbea
myounis007 <myounis4707@gmail.com>
2024-03-25 13:51:19 +0500

    crud operations basics in dart done

--- switch_case.dart
+++ switch_case.dart
@@ -1,3 +1,25 @@
+import 'dart:io';
+
 void main() {
-  
+  var num2=34;
+  var num1=23;
+  print('enter calculations sign');
+  var userInput=stdin.readLineSync();
+
+  switch (userInput) {
+    case '+':
+    print(num1+num2);
+      
+      break;
+    case 'x':
+    print(num1*num2);
+      
+      break;
+    case '=':
+    print(num1%num2);
+      
+      break;
+    default:
+    print('not supported');
+  }
 }
