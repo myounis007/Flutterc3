a283023dd7325349c66b6e6d08e8dc86d2cdec5a
myounis007 <myounis4707@gmail.com>
2024-03-26 09:53:18 +0500

    classes and object practice

--- enum.dart
+++ enum.dart
@@ -1,5 +1,20 @@
 void main() {
-  print('Enum in Dart');
-  print('============');
-  
+  // print('Enum in Dart');
+  // print('============');
+
+  Student student1 = Student();
+student1.name='unis';
+print(student1.name);
+}
+
+class Student {
+  late String name;
+
+  projectSubmit(bool isSubitted) {
+    if (isSubitted) {
+      print('$name submit the project');
+    } else {
+      print('$name not submitted');
+    }
+  }
 }
--- oop.dart
+++ oop.dart
@@ -1,26 +1,26 @@
+import 'dart:io';
 void main() {
   print('Object Oriented Programming in Dart');
    print('===================================');
   Student student1 = Student();
-  Student student2 = Student();
   student1.name = 'unis';
   student1.age = 24;
-  student1.projectsubmission(true);
-  print('===================================');
+  student1.projectsubmission();
+ 
 
-  student2.name = 'Ali';
-  student2.age = 34;
-  student2.projectsubmission(true);
-   print('===================================');
+
 }
 
 class Student {
   late String name;
+
   late int age;
 
-  projectsubmission(bool isSubmitted) {
-    if (isSubmitted) {
-      print('$name is submitted the project');
+  projectsubmission(){
+    print('please enter true or false');
+    var submitted= stdin.readLineSync()!;
+    if (submitted=="true") {
+      print('$name submitted the project');
     } else {
       print('$name ny project submit nai kiya');
     }
